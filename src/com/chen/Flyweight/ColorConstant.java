3915eafbf3b672b468004b1a0264ab02e5d4d3bd
LeifChen <leifchen90@gmail.com>
2017-09-19 20:48:02 +0800

    Add flyweight pattern

---
+++ src/com/chen/Flyweight/ColorConstant.java
@@ -0,0 +1,17 @@
+package com.chen.Flyweight;
+
+/**
+ * Color常量类
+ *
+ * @Author: LeifChen
+ * @Date: 2017-09-19
+ */
+public class ColorConstant {
+
+    public static final String COLOR_RED = "RED";
+
+    public static final String COLOR_GREEN = "GREEN";
+
+    public static final String COLOR_BULE = "BULE";
+
+}
---
+++ src/com/chen/Flyweight/Shape.java
@@ -0,0 +1,13 @@
+package com.chen.Flyweight;
+
+/**
+ * Shape接口
+ *
+ * @Author: LeifChen
+ * @Date: 2017-09-18
+ */
+public interface Shape {
+
+    void draw();
+
+}
