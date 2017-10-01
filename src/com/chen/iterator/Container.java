97de19df78e3b788f2f2226cc2f7dc585ba9ed4b
LeifChen <leifchen90@gmail.com>
2017-10-01 11:51:50 +0800

    Add iterator pattern

---
+++ src/com/chen/iterator/Container.java
@@ -0,0 +1,12 @@
+package com.chen.iterator;
+
+/**
+ * Container 接口
+ *
+ * @Author: LeifChen
+ * @Date: 2017-10-01
+ */
+public interface Container {
+
+    Iterator getIterator();
+}
---
+++ src/com/chen/iterator/Iterator.java
@@ -0,0 +1,15 @@
+package com.chen.iterator;
+
+/**
+ * Iterator 接口
+ *
+ * @Author: LeifChen
+ * @Date: 2017-10-01
+ */
+public interface Iterator {
+
+    boolean hasNext();
+
+    Object next();
+
+}
